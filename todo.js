b1f16005615305b4ef91eca3b1bdbf5220d91629
Praveenkumar <166112831+PRAVEENKUMARhicet@users.noreply.github.com>
2024-07-21 00:03:04 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,28 @@
+var input=document.getElementById("add-list");
+var todo=document.getElementById("list");
+var button=document.getElementById("add-button");
+
+button.addEventListener('click',()=>{
+    item(input.value)
+    input.value=''
+})
+
+function item(values){
+    let lists=document.createElement('p')
+    let btn=document.createElement('button')
+    if(values=="" || values==null){
+        alert("Enter an item")
+        return false;
+    }
+    else{
+        btn.textContent="delete"
+        let span=document.createElement('span')
+        span.textContent=values
+        lists.appendChild(span)
+        lists.appendChild(btn)
+        todo.appendChild(lists)
+    }
+    btn.addEventListener('click',()=>{
+        todo.removeChild(lists)   
+    })
+}
